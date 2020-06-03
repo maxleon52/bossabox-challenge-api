2dae3e25009a012fc0829dda320432a78de70032
maxleon52 <maxleon522@gmail.com>
2020-06-03 19:49:09 -0300

    little fix

--- src/server.js
+++ src/server.js
@@ -10,11 +10,8 @@ mongoose.connect(
 );
 
 const app = express();
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger.json");
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(process.env.PORT || 3333);
